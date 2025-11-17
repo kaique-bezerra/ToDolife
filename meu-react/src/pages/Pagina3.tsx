cb9ced268d696751a781a138f7c9dac0f2f039d5
kaique-bezerra <bezerrak114@gmail.com>
2025-11-17 20:52:02 -0300

    terminei a pagina 1 e a pagina 2 linkando para a proxima pagina so falta altera o endereço do link

--- meu-react/src/pages/Pagina3.tsx
+++ meu-react/src/pages/Pagina3.tsx
@@ -1,11 +1,61 @@
-import { Link } from "react-router-dom";
-import './Pagina3.css';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import minhaImagem from "../assets/ChatGPT Image 17 de nov. de 2025, 19_12_16.png";
+import "./Pagina3.css";
 
 function Pagina3() {
+
+  const navigate = useNavigate();
+
+  const textos = [
+    {
+      titulo: "Bem-vindo ao ToDolife",
+      subtitulo: "Transforme sua rotina em uma aventura épica."
+    },
+    {
+      titulo: "Seu dia, seu jogo!",
+      subtitulo: "Cada tarefa concluída te dá mais XP."
+    },
+    {
+      titulo: "Pronto para começar?",
+      subtitulo: "Você está a um clique de evoluir sua rotina."
+    }
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  function handleClick() {
+    // Se for o último clique → redireciona
+    if (index === textos.length - 1) {
+      navigate("/"); // ← Mude aqui para a página que você quiser
+      return;
+    }
+
+    // Senão → apenas troca os textos
+    setIndex(index + 1);
+  }
+
   return (
-    <div className="pagina3" >
-      <h1 className="titulo">Página 3</h1>
-      <p className="titulo">Você está na terceira página.</p>
+    <div className="pagina3">
+      <div className="logo-container">
+        <img src={minhaImagem} alt="Logo" className="logo" />
+      </div>
+
+      {/* PRIMEIRO TEXTO */}
+      <p className="titulo">
+        <b>{textos[index].titulo}</b>
+      </p>
+
+      {/* SEGUNDO TEXTO */}
+      <p className="titulo">
+        <b>{textos[index].subtitulo}</b>
+      </p>
+
+      <div className="buttons">
+        <button className="btn primary" onClick={handleClick}>
+          Continuar
+        </button>
+      </div>
 
       <Link to="/">Ir para Página 2</Link>
     </div>
